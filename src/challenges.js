1f99935dd35a7bcd26fff570fd51d767e005b09e
PedroSpelta <p.h.s.8989@hotmail.com>
2021-05-26 19:52:36 -0300

    Reduzindo erros no linter

--- src/challenges.js
+++ src/challenges.js
@@ -1,13 +1,13 @@
 // Desafio 1
-function compareTrue(a,b) {
+function compareTrue(a, b) {
   // seu código aqui
-  return a&&b;
+  return a && b;
 }
 
 // Desafio 2
-function calcArea(base,height) {
+function calcArea(base, height) {
   // seu código aqui
-  return base*height/2;
+  return (base * height) / 2;
 }
 
 // Desafio 3
@@ -19,19 +19,18 @@ function splitSentence(string) {
 // Desafio 4
 function concatName(array) {
   // seu código aqui
-  return `${array[array.length-1]}, ${array[0]}`;
+  return `${array[array.length - 1]}, ${array[0]}`;
 }
 
 // Desafio 5
 function footballPoints(wins, ties) {
   // seu código aqui
-  return wins*3+ties*1;
+  return wins * 3 + ties * 1;
 }
 
-
-function biggestNumber(array){
+function biggestNumber(array) {
   let result = array[0];
-  for (number of array){
+  for (let number of array) {
     if (number > result) result = number;
   }
   return result;
@@ -42,30 +41,28 @@ function highestCount(array) {
   // seu código aqui
   const biggest = biggestNumber(array);
   let count = 0;
-  for (number of array){
-    if (number == biggest) count +=1;
+  for (let number of array) {
+    if (number === biggest) count += 1;
   }
   return count;
 }
 
-
-
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
   // seu código aqui
-  if (Math.abs(cat1-mouse)<Math.abs(cat2-mouse)) return 'cat1'
-  else if (Math.abs(cat1-mouse)>Math.abs(cat2-mouse)) return 'cat2'
-  else return 'os gatos trombam e o rato foge'
+  if (Math.abs(cat1 - mouse) < Math.abs(cat2 - mouse)) return 'cat1';
+  if (Math.abs(cat1 - mouse) > Math.abs(cat2 - mouse)) return 'cat2';
+  return 'os gatos trombam e o rato foge';
 }
 
 // Desafio 8
 function fizzBuzz(array) {
   // seu código aqui
   let result = [];
-  for (number of array){
-    if (number%3==0 && number%5==0) result.push('fizzBuzz');
-    else if (number%3==0) result.push('fizz');
-    else if (number%5==0) result.push('buzz');
+  for (let number of array) {
+    if (number % 3 === 0 && number % 5 === 0) result.push('fizzBuzz');
+    else if (number % 3 === 0) result.push('fizz');
+    else if (number % 5 === 0) result.push('buzz');
     else result.push('bug!');
   }
   return result;
@@ -74,21 +71,21 @@ function fizzBuzz(array) {
 // Desafio 9
 function encode(string) {
   // seu código aqui
-  let final = string
+  let final = string;
   let vowels = 'aeiou';
-  for (let letter of vowels){
-    let re = new RegExp(`${letter}`,'g')
-    final = final.replace(re,`${vowels.indexOf(letter)+1}`)
+  for (let letter of vowels) {
+    let re = new RegExp(`${letter}`, 'g');
+    final = final.replace(re, `${vowels.indexOf(letter) + 1}`);
   }
   return final;
 }
 function decode(string) {
   // seu código aqui
-  let final = string
+  let final = string;
   let vowels = 'aeiou';
-  for (let letter in vowels){
-    let re = new RegExp(`${parseInt(letter)+1}`,'g')
-    final = final.replace(re,`${vowels[parseInt(letter)]}`)
+  for (let letter in vowels) {
+    let re = new RegExp(`${parseInt(letter) + 1}`, 'g');
+    final = final.replace(re, `${vowels[parseInt(letter)]}`);
   }
   return final;
 }
--- src/challenges2.js
+++ src/challenges2.js
@@ -1,33 +1,32 @@
 // Desafio 10
-function techList(array,name) {
+function techList(array, name) {
   // seu código aqui
   let final = [];
-  array = array.sort()
-  if (array.length==0) return 'Vazio!'
-  for (let index in array){
-    let obj = {}
-    obj['tech'] = array[parseInt(index)];
-    obj['name'] = name;
+  array = array.sort();
+  if (array.length === 0) return 'Vazio!';
+  for (let index in array) {
+    let obj = {};
+    obj.tech = array[parseInt(index)];
+    obj.name = name;
     final.push(obj);
   }
-  return final
+  return final;
 }
 
 // Desafio 11
-function checkNumber(array){
-  let biggerCount = 0;
-  if (array.length !=11) return [false,'Array com tamanho incorreto.']
-  for (number of array){
+function checkNumber(array) {
+  if (array.length != 11) return [false, 'Array com tamanho incorreto.'];
+  for (number of array) {
     let actualCount = 0;
-    if (number<0 || number >10) return [false,'não é possível gerar um número de telefone com esses valores'];
-    for (number2 of array){
-      if (number2==number) {
-        actualCount +=1;
-        if (actualCount>2) return [false,'não é possível gerar um número de telefone com esses valores'];
+    if (number < 0 || number > 10) return [false, 'não é possível gerar um número de telefone com esses valores'];
+    for (number2 of array) {
+      if (number2 === number) {
+        actualCount += 1;
+        if (actualCount > 2) return [false, 'não é possível gerar um número de telefone com esses valores'];
       }
     }
   }
-  return [true]
+  return [true];
 }
 
 function generatePhoneNumber(array) {
@@ -35,18 +34,18 @@ function generatePhoneNumber(array) {
   let check = checkNumber(array);
   if (!(check[0])) return check[1];
   let finalNumber = '(';
-  for (number of array){
-    finalNumber+=number
-    if (finalNumber.length == 3) finalNumber+=') '
-    else if (finalNumber.length == 10) finalNumber+='-'
+  for (let number of array) {
+    finalNumber += number;
+    if (finalNumber.length === 3) finalNumber += ') ';
+    else if (finalNumber.length === 10) finalNumber += '-';
   }
-  return finalNumber
+  return finalNumber;
 }
 
 // Desafio 12
 function triangleCheck() {
   // seu código aqui
-  
+
 }
 
 // Desafio 13
